e077950f3a552510a1f2e7a665af2c71e029f5da
ProgramEater <stepanov.s2006@yandex.ru>
2024-05-19 20:39:03 +0300

    1.0.1

--- src/main/java/org/server/fiftybees/App.java
+++ src/main/java/org/server/fiftybees/App.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @SpringBootApplication
@@ -21,17 +22,19 @@ public class App {
         UserRepository userRepository =
                 configurableApplicationContext.getBean(UserRepository.class);
 
-        authorityRepository.save(new Authority("ROLE_ADMIN"));
-        authorityRepository.save(new Authority("ROLE_USER"));
+        if (!(authorityRepository.findByAuthority("ROLE_ADMIN").isPresent())) {authorityRepository.save(new Authority("ROLE_ADMIN"));}
+        if (!(authorityRepository.findByAuthority("ROLE_USER").isPresent())) {authorityRepository.save(new Authority("ROLE_USER"));}
 
-        // admin user
-        User admin_user = new User("admin", "111@ya.ru", "$2a$10$eObqG4zk7CbKPPTv0daHm.bcpK6zwyFPpjAVXOeDWrT/3TpVcxpia");
-        // get admin role
-        Set<Authority> authority = new HashSet<>();
-        authority.add(authorityRepository.findByAuthority("ROLE_ADMIN").get());
-        admin_user.setAuthorities(authority);
+        if (!(userRepository.findByUsername("admin").isPresent())) {
+            // admin user
+            User admin_user = new User("admin", "111@ya.ru", "$2a$10$eObqG4zk7CbKPPTv0daHm.bcpK6zwyFPpjAVXOeDWrT/3TpVcxpia");
+            // get admin role
+            Set<Authority> authority = new HashSet<>();
+            authority.add(authorityRepository.findByAuthority("ROLE_ADMIN").get());
+            admin_user.setAuthorities(authority);
 
-        userRepository.save(admin_user);
+            userRepository.save(admin_user);
+        }
 
     }
 }
